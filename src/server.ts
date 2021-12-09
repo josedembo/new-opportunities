d8ca9c1c1618fc0f021926f9043cbf966fdee57f
josedembo <josedembo18@aluno.unilab.edu.br>
2021-12-09 08:26:25 -0300

    renomeia o arquivo ormConfig.json

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,9 @@ import express, { response } from "express";
 const app = express();
 
 app.get("/", (request, response) => {
-    response.send("Hello world newOpportunities");
+    response.send("Hello world newOpportunities")
 });
 
 app.listen(3333, () => {
     console.log("server is ronning at port 3333");
-});
+})
