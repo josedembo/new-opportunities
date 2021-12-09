fb7a698f9b114d0d7c1b07a00820c8a914016a78
josedembo <josedembo18@aluno.unilab.edu.br>
2021-12-09 11:57:51 -0300

    adiciona o middleware de autenticacão
        - implementa a lógica do middleware de autentitcacão do usuario (userAuthentication)

---
+++ src/@types/express/index.d.ts
@@ -0,0 +1,11 @@
+declare namespace Express {
+
+    interface Request {
+
+        user: {
+            id: string
+            name: string
+            email: string
+        }
+    }
+}
---
+++ src/middlewares/userAuthentiation.ts
@@ -0,0 +1,57 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "../model/errors/AppErros";
+import { verify } from "jsonwebtoken";
+import jwtConfig from "../config/auth";
+
+interface ItokenPayload {
+    name: string;
+    email: string;
+    iat: number;
+    exp: number;
+    sub: string;
+}
+
+interface IUser {
+
+    id: string;
+    name: string;
+    email: string
+
+}
+
+function userAuthentication(request: Request, response: Response, next: NextFunction) {
+
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+        throw new AppError("anouthorizad", 401);
+    }
+
+    const [, token] = authHeader.split(" ");
+
+
+    const { secret } = jwtConfig.jwt;
+
+    try {
+
+        const verifingToken = verify(token, secret);
+
+        const { sub, email, name } = verifingToken as ItokenPayload;
+
+        const user: IUser = {
+            id: sub,
+            name,
+            email
+        }
+
+        request.user = user;
+
+        next();
+
+    } catch (error) {
+        throw new AppError("invalid token", 401);
+    }
+
+}
+
+export { userAuthentication }
