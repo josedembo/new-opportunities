e9388ce40d92a9c0e6eae4d9af6ccdaea2f32fe4
josedembo <josedembo18@aluno.unilab.edu.br>
2021-12-13 09:13:29 -0300

    deleta a pasta build

--- build/useCases/user/getAll/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAllUserController = void 0;
-const GetAllUserController_1 = require("./GetAllUserController");
-const getAllUserController = new GetAllUserController_1.GetAllUserController();
-exports.getAllUserController = getAllUserController;
--- build/useCases/user/update/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.updateUserController = void 0;
-const UpdateUserController_1 = require("./UpdateUserController");
-const updateUserController = new UpdateUserController_1.UpdateUserControlller();
-exports.updateUserController = updateUserController;
