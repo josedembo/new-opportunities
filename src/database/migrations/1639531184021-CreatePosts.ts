6e6f084b51043e307cbe1b429f6faeda804ac70b
josedembo <josedembo18@aluno.unilab.edu.br>
2021-12-14 22:53:04 -0300

    Implementa os métodos up e down da migration(Post)

--- src/database/migrations/1639531184021-CreatePosts.ts
+++ src/database/migrations/1639531184021-CreatePosts.ts
@@ -1,11 +1,63 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreatePosts1639531184021 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.createTable(
+            new Table({
+                name: "posts",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true,
+                        generationStrategy: "uuid"
+
+                    },
+                    {
+                        name: "title",
+                        type: "varchar"
+                    },
+                    {
+                        name: "type",
+                        type: "varchar"
+                    },
+                    {
+                        name: "description",
+                        type: "varchar"
+                    },
+                    {
+                        name: "user",
+                        type: "uuid"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ],
+                foreignKeys: [
+                    {
+                        name: "FKUserCreatePost",
+                        referencedTableName: "users",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["user"],
+                        onDelete: "SET NULL",
+                        onUpdate: "SET NULL",
+                    }
+                ]
+            })
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("posts");
+
     }
 
 }
